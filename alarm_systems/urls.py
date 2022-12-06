1819a9a7a04fa75a90fcb4f7e15c56baaa86f5d3
MarcinSerwinski <MarcinSerwinski@github.com>
2022-12-06 14:23:24 +0100

    added main view to present informations about customers

--- alarm_systems/urls.py
+++ alarm_systems/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from . import views
+from .views import MainView
 
 app_name = 'alarm_systems'
 
 urlpatterns = [
-path('', views.home, name='home')
-]
+    path('', views.home, name='home'),
+    path('main_view/', MainView.as_view(), name='main_view'),
 
+]
--- alarm_systems/views.py
+++ alarm_systems/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.views import View
 
+from alarm_systems.models import Customer
 
 
 # Create your views here.
@@ -7,3 +10,14 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'home/home.html')
+
+
+class MainView(View):
+    def get(self, request):
+        customers_all = Customer.objects.all().order_by('last_name')
+        return render(
+            request,
+            'home/alarm_systems_main_view.html',
+            context={
+                'customers_all': customers_all
+            })
