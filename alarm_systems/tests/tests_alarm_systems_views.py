67de7dbcd6a225d09a6235e361ea4b8366645a9d
MarcinSerwinski <MarcinSerwinski@github.com>
2022-12-13 16:08:02 +0100

    added next unit tests and fixtures

--- alarm_systems/tests/tests_alarm_systems_views.py
+++ alarm_systems/tests/tests_alarm_systems_views.py
@@ -1,18 +1,12 @@
-import os
-import uuid
-
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission, Group
-from django.test import TestCase
-
+from django.contrib.auth.models import Permission
 import pytest
-
 from django.urls import reverse
 
-from alarm_systems.models import Customer
+from alarm_systems.forms import AddCustomerForm, DeleteCustomerForm
+from alarm_systems.models import Customer, Location
 
 
-def test_home_page(client):
+def test_home_page_get(client):
     endpoint = reverse('alarm_systems:home')
     response = client.get(endpoint)
     assert response.status_code == 200
@@ -37,16 +31,113 @@ def test_acces_with_permission_to_mainview(client, user):
     assert '<h1>All customers listed below:</h1>' in str(response.content)
 
 
-def test_add_customer_page(client):
+def test_add_customer_get(client):
     endpoint = reverse('alarm_systems:add_customer')
     response = client.get(endpoint)
+    form_in_view = response.context['form']
     assert response.status_code == 200
+    assert isinstance(form_in_view, AddCustomerForm)
     assert '<button type="submit" class="btn btn-success">Add new customer</button>' in str(response.content)
 
 
-def test_send_email_page(db, client):
-    customer = Customer.objects.create()
-    endpoint = reverse('alarm_systems:send_email', kwargs={'customer_id': customer.pk})
+def test_add_customer_post(db, client):
+    form_url = reverse('alarm_systems:add_customer')
+    data = {'first_name': 'TestFirstName', 'last_name': 'TestLastName',
+            'address': 'Test, Address 1',
+            'email': 'form@test.com', 'phone_number': '123456789',
+            'description': 'Test1'}
+    response = client.post(form_url, data)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('alarm_systems:main_view'))
+    assert Customer.objects.get(first_name='TestFirstName', description='Test1')
+
+
+def test_delete_customer_get(db, client, customer_id):
+    endpoint = reverse('alarm_systems:delete_customer', kwargs={'customer_id': customer_id})
+    response = client.get(endpoint)
+    form_in_view = response.context['form']
+    assert response.status_code == 200
+    assert isinstance(form_in_view, DeleteCustomerForm)
+    assert '<button type="submit" class="btn btn-outline-danger">Yes. Delete.</button>' in str(response.content)
+
+
+def test_delete_customer_post(db, client, customer_id):
+    form_url = reverse('alarm_systems:delete_customer', kwargs={'customer_id': customer_id})
+    response = client.post(form_url)
+    customer = Customer.objects.all()
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('alarm_systems:main_view'))
+    assert len(customer) == 0
+
+
+def test_edit_customer_get(db, client, customer_id):
+    endpoint = reverse('alarm_systems:edit_customer_all_view', kwargs={'pk': customer_id})
     response = client.get(endpoint)
     assert response.status_code == 200
-    assert '<button type="submit" class="btn btn-success">Send an email</button>' in str(response.content)
+    assert '<button type="submit" value="Update" class="btn btn-success">Edit</button>' in str(response.content)
+
+
+def test_edit_customer_post(db, client, customer_id):
+    form_url = reverse('alarm_systems:edit_customer_all_view', kwargs={'pk': customer_id})
+    data = {'first_name': 'EditedFirstName', 'last_name': 'TestLastName',
+            'address': 'Test, Address 1',
+            'email': 'form@test.com', 'phone_number': 'edited123456789',
+            'description': 'Test1'}
+    response = client.post(form_url, data)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('alarm_systems:main_view'))
+    assert Customer.objects.get(first_name='EditedFirstName', phone_number='edited123456789')
+
+
+def test_details_customer_get(db, client, customer_id):
+    endpoint = reverse('alarm_systems:details_customer', kwargs={'customer_id': customer_id})
+    response = client.get(endpoint)
+    customer_in_view = response.context['customer']
+    assert customer_in_view.id in response.context
+    assert response.status_code == 200
+    assert "<h1>Locations listed below:</h1>" in str(response.content)
+
+
+def test_add_location_get(db, client, customer_id, location_customers_id):
+    endpoint = reverse('alarm_systems:add_location_customer', kwargs={'customer_id': customer_id})
+    response = client.get(endpoint)
+    assert response.status_code == 200
+    assert '<button type="submit" class="btn btn-success">Add new ' in str(response.content)
+
+
+def test_add_location_post(db, client, customer_id):
+    form_url = reverse('alarm_systems:add_location_customer', kwargs={'customer_id': customer_id})
+    data = {'name': 'TestLocationName', 'address': 'Test Location, Address 2', 'description': 'Test2'}
+    response = client.post(form_url, data)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('alarm_systems:details_customer', kwargs={'customer_id': customer_id}))
+    assert Location.objects.get(name='TestLocationName', description='Test2')
+
+
+def test_edit_location_get(db, client, customer_id, location_customers_id):
+    endpoint = reverse('alarm_systems:edit_location', kwargs={'pk': customer_id})
+    response = client.get(endpoint)
+    assert response.status_code == 200
+    assert '<h1>Edit Location</h1>' in str(response.content)
+
+
+def test_edit_location_post(db, client, customer_id, location_customers_id):
+    form_url = reverse('alarm_systems:edit_location', kwargs={'pk': customer_id})
+    data = {'name': 'TestLocationEditedName', 'address': 'Edited Test Location, Address 2', 'description': 'Test2'}
+    response = client.post(form_url, data)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('alarm_systems:details_customer', kwargs={'customer_id': customer_id}))
+    assert Location.objects.get(name='TestLocationEditedName', address='Edited Test Location, Address 2')
+
+
+
+
+
+
+
+
+# def test_send_email_page(db, client, customer_id):
+#     endpoint = reverse('alarm_systems:send_email', kwargs={'customer_id': customer_id})
+#     response = client.get(endpoint)
+#     assert response.status_code == 200
+#     assert '<button type="submit" class="btn btn-success">Send an email</button>' in str(response.content)
--- conftest.py
+++ conftest.py
@@ -1,7 +1,44 @@
 import pytest
 
+from alarm_systems.models import Customer, Location
+
 
 @pytest.fixture(scope='function')
 def user(db, django_user_model):
     """Create django user"""
     yield django_user_model.objects.create_user(email='test2@admin.com', fullname='Test User', password='TestPass123')
+
+
+# @pytest.fixture()
+# def user_with_permission(db, django_user_model):
+#     django_user_model.objects.create_user(email='test3@admin.com', fullname='Test User Permission',
+#                                           password='TestPass123')
+#
+
+@pytest.fixture()
+def create_customer():
+    customer = Customer.objects.create()
+    yield customer
+
+
+@pytest.fixture()
+def customer_id():
+    customer_id = (Customer.objects.create()).id
+    yield customer_id
+
+
+@pytest.fixture()
+def create_location():
+    location = Location.objects.create()
+    yield location
+
+
+@pytest.fixture()
+def location_id():
+    location_id = (Location.objects.create()).id
+    yield location_id
+
+@pytest.fixture()
+def location_customers_id(customer_id):
+    location_customers_id = Location.objects.create(customer_id=customer_id)
+    yield location_customers_id
